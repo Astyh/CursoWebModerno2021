550a33576c3c3e975d06f4e3b89971e4ed180251
Astyh <rafael_system_metal@yahoo.com.br>
2021-07-22 13:50:43 -0300

    React Exercicios e Projetos Finalizados

---
+++ exercicios-web/react/crud/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> Rafa<span className="text-danger">3</span>l</strong>
+        </span>
+    </footer>
