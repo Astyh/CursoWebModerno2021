04280b0da2a2e625b301c11ca6fad3250fcdf214
rafaellAndrade <rafael_system_metal@yahoo.com.br>
2021-11-08 12:31:55 -0300

    Aulao NextJs

---
+++ exercicios-web/aulao_nextjs/fundamentos/src/pages/jsx.jsx
@@ -0,0 +1,20 @@
+import Layout from "../components/Layout"
+export default function Jsx(){
+    const titulo = <h1>JSX é um Conceito Central</h1>
+
+    function subtitulo() {
+        return <h2>{"Muito legal".toUpperCase()}</h2>        
+    }
+
+    return (
+        <Layout titulo="Entendendo o JSX">            
+            <div>
+                {titulo}
+                {subtitulo()}         
+                <p>
+                    {JSON.stringify({nome: 'João', "idade": 30})}
+                </p>
+            </div>
+        </Layout>
+    )
+}
